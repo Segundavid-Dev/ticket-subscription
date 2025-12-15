cb411a6d2c6db71bfebd2ec44e508985d8077022
Segundavid-Dev <segdavid03@gmail.com>
2025-12-15 11:04:29 +0100

    defined model for subscription

--- server/src/models/subscription.model.js
+++ server/src/models/subscription.model.js
@@ -1,3 +1,81 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({});
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Subscription name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: [true, "Subscription price is required"],
+      min: [0, "Price cannot be negative"],
+      currency: {
+        type: String,
+        required: [true, "Currency is required"],
+        enum: ["USD", "EUR", "GBP", "INR", "NGN"],
+        default: "USD",
+      },
+      frequency: {
+        type: String,
+        enum: ["daily", "weekly", "monthly", "yearly"],
+      },
+      category: {
+        type: String,
+        enum: [
+          "sports",
+          "news",
+          "entertainment",
+          "lifestyle",
+          "technology",
+          "finance",
+          "politics",
+          "other",
+        ],
+        required: [true, "Category is required"],
+      },
+      paymethod: {
+        type: String,
+        required: [true, "Payment method is required"],
+        trim: true,
+      },
+      status: {
+        type: String,
+        enum: ["active", "expired", "cancelled"],
+        default: "active",
+      },
+      startDate: {
+        type: Date,
+        required: [true, "Start date is required"],
+        validate: {
+          validator: (value) => value <= new Date(),
+          message: "Start date must be in the past",
+        },
+        renewalDate: {
+          type: Date,
+          required: [true, "Start date is required"],
+          validate: {
+            validator: function (value) {
+              return value > this.startDate;
+            },
+            message: "Renewal date must be after start date",
+          },
+        },
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: [true, "User is required"],
+          index: true,
+        },
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default Subscription;
